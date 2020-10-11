5ecaf7faf2fb024b1ab517113714fd2968752c6b
Teresa <miss.teresa.feng@gmail.com>
2020-10-12 10:16:41 +1100

    Update AuthContext.tsx

--- frontend/src/AuthContext.tsx
+++ frontend/src/AuthContext.tsx
@@ -35,34 +35,4 @@ export const AuthProvider = ({ children }: React.PropsWithChildren<{}>) => {
   return <AuthContext.Provider value={store}>{children}</AuthContext.Provider>;
 };
 
-// export const AuthProvider: React.FC = (props: React.PropsWithChildren<{}>) => {
-//   const [isAuth, setIsAuth] = React.useState(false);
-
-//   const login = action(() => {
-//     // setting timeout to mimic an async login
-//     console.log("login");
-//     setTimeout(() => setIsAuth(true), 1000);
-//   });
-//   const logout = () => setIsAuth(false);
-
-//   return (
-//     <AuthContext.Provider
-//       value={{
-//         isAuth: isAuth,
-//         login: login,
-//         logout: logout,
-//       }}
-//     >
-//       {props.children}
-//     </AuthContext.Provider>
-//   );
-// };
-// export const AuthConsumer = AuthContext.Consumer;
-
 export const useStore = () => React.useContext(AuthContext);
-
-/* HOC to inject store to any functional or class component */
-// eslint-disable-next-line react/display-name
-export const withStore = (Component: React.ComponentType) => (props: any) => {
-  return <Component {...props} store={useStore()} />;
-};
