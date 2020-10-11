fb1d08c2b317f2ad24d9003820cea2a7ae89c366
Jeremy Chiu <jeremy.chiu26@gmail.com>
2020-10-11 21:33:06 +1100

    implemented basic page structure

--- frontend/src/view_listing/listingPage.css.tsx
+++ frontend/src/view_listing/listingPage.css.tsx
@@ -3,44 +3,47 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core";
 export const listingPageStyle = makeStyles((theme: Theme) =>
   createStyles({
     page: {
-      padding: theme.spacing(2, "15%", 0, "15%"),
-      display: "flex",
-      flexDirection: "column",
+        padding: theme.spacing(2, "15%", 0, "15%"),
+        display: "flex",
+        flexDirection: "column",
     },
     backButton: {
-      width: "fit-content",
+        width: "fit-content",
     },
-    streetAddress: {
-      textTransform: "capitalize",
-      marginTop: theme.spacing(2),
+    header: {
+        margin: theme.spacing(4, 0, 0, 0),
     },
-    secondaryAddress: {
-      textTransform: "capitalize",
-      color: theme.palette.grey[500],
+    divider: {
+        border: theme.palette.grey[700] + " solid 1px ",
+        backgroundColor: theme.palette.grey[700],
+        margin: theme.spacing(0, 0, 1, 0),
     },
-    auctionTime: {
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: theme.spacing(3, 0, 5, 0),
+    imageContainer: {
+        width: "100%",
+        height: "300px",
+        objectFit: "contain",
+        backgroundColor: "rgba(0,0,0, 0.8)",
     },
-    auctionText: {
-      marginBottom: theme.spacing(1),
-      color: theme.palette.grey[700],
+    sliderContainer: {
+        [theme.breakpoints.up("md")]: {
+          width: "100%",
+        },
+        height: "300px",
     },
-    mainImage: {
-      width: "100%",
-      height: "100%",
-      backgroundPosition: "center",
-      backgroundSize: "cover",
+    street: {
+        padding: theme.spacing(2, 0, 0, 0),
     },
-    biddingInfo: {
-      marginTop: theme.spacing(3),
+    title: {
+        margin: theme.spacing(1, 0, 1, 0),
     },
-    auctionClosed: {
-      color: theme.palette.error.main,
-      padding: theme.spacing(3, 0, 3, 0),
+    description: {
+        margin: theme.spacing(0, 0, 3, 0),
     },
+    detailBar: {
+        display: "flex",
+        margin: theme.spacing(1, 0, 0, 0),
+        justifyContent: "flex-end",
+        padding: theme.spacing(2, 0, "15%", 0),
+    }
   })
 );
--- frontend/src/view_listing/listingPage.stories.tsx
+++ frontend/src/view_listing/listingPage.stories.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react/types-6-0";
-import { BiddersList } from "../auction/bidders_list/BiddersList";
 import { ListingPage } from "./listingPage";
-import { Property } from "./listingPage";
-import imageSlideshow from "../images/propertyMain.jpg";
-
+import { createFakeListing } from "../ui/util/fakes/listing"
 export default {
   title: "view_listing/listingPage",
-  component: BiddersList,
+  component: ListingPage,
+  argTypes: {
+    auction_start: {
+      control: {
+        type: "date",
+      },
+      defaultValue: createFakeListing().auction_start,
+    },
+    auction_end: {
+      control: {
+        type: "date",
+      },
+      defaultValue: createFakeListing().auction_end,
+    },
+  },
 } as Meta;
 
-const Template: Story<{
-    // pass in property information through props
-    // arguments to be passed into listing page
-    property: Property;
-}> = (props: {property: Property;}) => {
-    
-    return (
-        <ListingPage
-            // pass in arguments 
-            property={props.property}
-            images={imageSlideshow}
-        />
-    );
-};
-
-export const Overview = Template.bind({});
+export const Overview = (props: {
+    auction_start: string;
+    auction_end: string;
+  }) => (
+    <ListingPage
+      listing={createFakeListing({
+        auction_start: new Date(props.auction_start),
+        auction_end: new Date(props.auction_end),
+      })}
+    />
+  );
