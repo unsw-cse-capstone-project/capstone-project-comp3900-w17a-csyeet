9d01118eefabf93b1eeb35713ee28a30b96b5c80
Teresa Feng <teresa.f@canva.com>
2020-10-11 11:48:30 +1100

    added property and fake type

---
+++ frontend/src/ui/util/types/property.ts
@@ -0,0 +1,21 @@
+import { type } from "os";
+
+export type Property = {
+  id: number;
+  owner_id: number;
+  title: string;
+  description: string;
+  street: string;
+  suburb: string;
+  postcode: string;
+  state: string;
+  country: string;
+  type: string;
+  num_bedrooms: number;
+  num_bathrooms: number;
+  num_car_spaces: number;
+  auction_start: Date;
+  auction_end: Date;
+  reserve_price: number;
+  images: string[];
+};
