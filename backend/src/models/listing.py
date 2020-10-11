e0c46f115a1909360054e346209520c3bbb077b6
edzobel491 <edzo491@gmail.com>
2020-10-12 01:33:22 +1100

    Converts backend to FastAPI (#10)
    * Migration to FastAPI
    
    * Converts Listings to FastAPI
    
    * Adds launch.json for debugging
    
    Co-authored-by: Eddie Belokopytov <eddie_491@hotmail.com>

---
+++ backend/src/models/listing.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean, Enum, Text
+from sqlalchemy.orm import relationship
+from .base import Base
+from ..schemas import ListingType
+
+
+@dataclass
+class Listing(Base):
+    __tablename__ = 'listing'
+
+    id: int = Column(Integer, primary_key=True)
+    owner_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    type: ListingType = Column(Enum(ListingType), nullable=False)
+    title: str = Column(String(), nullable=False)
+    description: str = Column(Text, nullable=False)
+    street: str = Column(String(), nullable=False)
+    suburb: str = Column(String(), nullable=False)
+    postcode: str = Column(String(), nullable=False)
+    state: str = Column(String(), nullable=False)
+    country: str = Column(String(), nullable=False)
+    num_bedrooms: int = Column(Integer, nullable=False)
+    num_bathrooms: int = Column(Integer, nullable=False)
+    num_car_spaces: int = Column(Integer, nullable=False)
+    auction_start: datetime = Column(DateTime, nullable=False)
+    auction_end: datetime = Column(DateTime, nullable=False)
+    has_ensuite: bool = Column(Boolean, default=False, nullable=False)
+    has_built_in_wardrobe: bool = Column(
+        Boolean, default=False, nullable=False)
+    has_bathtub: bool = Column(Boolean, default=False, nullable=False)
+    is_furnished: bool = Column(Boolean, default=False, nullable=False)
+    has_open_kitchen: bool = Column(Boolean, default=False, nullable=False)
+    has_separate_kitchen: bool = Column(Boolean, default=False, nullable=False)
+    has_island_kitchen: bool = Column(Boolean, default=False, nullable=False)
+    has_gas_stove: bool = Column(Boolean, default=False, nullable=False)
+    has_electric_stove: bool = Column(Boolean, default=False, nullable=False)
+    has_induction_stove: bool = Column(Boolean, default=False, nullable=False)
+    has_balcony: bool = Column(Boolean, default=False, nullable=False)
+    has_ocean_view: bool = Column(Boolean, default=False, nullable=False)
+    has_bbq: bool = Column(Boolean, default=False, nullable=False)
+    has_porch: bool = Column(Boolean, default=False, nullable=False)
+    has_pool: bool = Column(Boolean, default=False, nullable=False)
+    has_gym: bool = Column(Boolean, default=False, nullable=False)
+
+    owner = relationship('User', back_populates='listings')
+
+    def __repr__(self):
+        return f"<Listing: {self.title}>"
---
+++ backend/src/schemas/listing.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from fastapi_utils.enums import CamelStrEnum
+from enum import auto
+from typing import List
+from .user import UserResponse
+
+
+class ListingType(CamelStrEnum):
+    house = auto()
+    apartment = auto()
+    townhouse = auto()
+    studio = auto()
+    duplex = auto()
+
+
+class Feature(CamelStrEnum):
+    ensuite = auto()
+    built_in_wardrobe = auto()
+    bathtub = auto()
+    furnished = auto()
+    open_kitchen = auto()
+    separate_kitchen = auto()
+    island_kitchen = auto()
+    gas_stove = auto()
+    electric_stove = auto()
+    induction_stove = auto()
+    balcony = auto()
+    ocean_view = auto()
+    bbq = auto()
+    porch = auto()
+    pool = auto()
+    gym = auto()
+
+
+field_to_feature_map = {
+    'has_ensuite': Feature.ensuite,
+    'has_built_in_wardrobe': Feature.built_in_wardrobe,
+    'has_bathtub': Feature.bathtub,
+    'is_furnished': Feature.furnished,
+    'has_open_kitchen': Feature.open_kitchen,
+    'has_separate_kitchen': Feature.separate_kitchen,
+    'has_island_kitchen': Feature.island_kitchen,
+    'has_gas_stove': Feature.gas_stove,
+    'has_electric_stove': Feature.electric_stove,
+    'has_induction_stove': Feature.induction_stove,
+    'has_balcony': Feature.balcony,
+    'has_ocean_view': Feature.ocean_view,
+    'has_bbq': Feature.bbq,
+    'has_porch': Feature.porch,
+    'has_pool': Feature.pool,
+    'has_gym': Feature.gym,
+}
+
+
+class ListingBase(BaseModel):
+    type: ListingType
+    title: str
+    description: str
+    street: str
+    suburb: str
+    postcode: str
+    state: str
+    country: str
+    num_bedrooms: int = Field(..., ge=0)
+    num_bathrooms: int = Field(..., ge=0)
+    num_car_spaces: int = Field(..., ge=0)
+    auction_start: datetime
+    auction_end: datetime
+    features: List[Feature]
+
+
+class CreateListingRequest(ListingBase):
+    pass
+
+
+class ListingResponse(ListingBase):
+    id: str
+    owner: UserResponse
