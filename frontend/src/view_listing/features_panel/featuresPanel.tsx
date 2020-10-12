f20a1da4279182549fe93ab439f12c3615957a0c
Jeremy Chiu <jeremy.chiu26@gmail.com>
2020-10-12 12:45:21 +1100

    changed default state for accordian panels to be expanded

--- frontend/src/view_listing/features_panel/featuresPanel.tsx
+++ frontend/src/view_listing/features_panel/featuresPanel.tsx
@@ -6,8 +6,20 @@ import { Typography } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export const FeaturesPanel = ({ features }: { features: string }) => {
+  const [expanded, setExpanded] = React.useState<string | false>("panel1");
+
+  const handleChange = (panel: string) => (
+    event: React.ChangeEvent<{}>,
+    newExpanded: boolean
+  ) => {
+    setExpanded(newExpanded ? panel : false);
+  };
   return (
-    <Accordion>
+    <Accordion
+      square
+      expanded={expanded === "panel1"}
+      onChange={handleChange("panel1")}
+    >
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1a-content"
