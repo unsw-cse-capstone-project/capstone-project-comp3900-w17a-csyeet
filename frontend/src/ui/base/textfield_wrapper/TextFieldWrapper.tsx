6d116b17749ea846e5c9ff876ce1e75984a9aa4c
Jwen00 <jenniferwen00@gmail.com>
2020-10-13 03:51:46 +1100

    Added signin/signout forms

--- frontend/src/ui/base/textfield_wrapper/TextFieldWrapper.tsx
+++ frontend/src/ui/base/textfield_wrapper/TextFieldWrapper.tsx
@@ -1,33 +1,41 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import { InputAdornment } from "@material-ui/core";
 export interface TextFieldWrapperProps {
   field: string;
   label: string;
   onChange: (value: string, field: string) => void;
+  type?: string;
   adornment?: React.ReactNode;
+  value?: string;
 }
 
 const TextFieldWrapper: React.FC<TextFieldWrapperProps> = ({
   field,
   label,
   onChange,
+  type = "text",
   adornment = null,
+  value = "",
 }) => {
-  const [value, setValue] = React.useState("");
+  const [v, setValue] = React.useState(value);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.value, field);
     setValue(e.target.value);
   };
   return (
-    <div style={{ marginTop: "5px", marginBottom: "5px" }}>
+    <div style={{ marginTop: "10px", marginBottom: "10px" }}>
       <TextField
         fullWidth
         variant="outlined"
-        value={value}
+        value={v}
         label={label}
+        type={type}
         onChange={handleChange}
         InputProps={{
-          endAdornment: adornment,
+          endAdornment: (
+            <InputAdornment position="end">{adornment}</InputAdornment>
+          ),
         }}
       />
     </div>
