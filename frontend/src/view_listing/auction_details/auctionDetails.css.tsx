3f8ce9858f95d625a8a698164a3435a25ae37ffa
jeremyyg <46275030+jeremyyg@users.noreply.github.com>
2020-10-14 10:27:47 +1100

    Update frontend/src/view_listing/auction_details/auctionDetails.css.tsx
    Co-authored-by: Teresa <miss.teresa.feng@gmail.com>

--- frontend/src/view_listing/auction_details/auctionDetails.css.tsx
+++ frontend/src/view_listing/auction_details/auctionDetails.css.tsx
@@ -8,7 +8,7 @@ export const auctionDetailsStyle = makeStyles((theme: Theme) =>
     divider: {
       border: theme.palette.grey[700] + " solid 1px ",
       backgroundColor: theme.palette.grey[700],
-      margin: theme.spacing(0, 0, 1, 0)
+      marginBottom: theme.spacing(1)
     }
   })
 );
