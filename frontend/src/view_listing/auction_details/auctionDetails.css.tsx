09cbb549b3946996e959e5db63a4102283f6cec8
jeremyyg <46275030+jeremyyg@users.noreply.github.com>
2020-10-14 10:27:40 +1100

    Update frontend/src/view_listing/auction_details/auctionDetails.css.tsx
    Co-authored-by: Teresa <miss.teresa.feng@gmail.com>

--- frontend/src/view_listing/auction_details/auctionDetails.css.tsx
+++ frontend/src/view_listing/auction_details/auctionDetails.css.tsx
@@ -3,7 +3,7 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core";
 export const auctionDetailsStyle = makeStyles((theme: Theme) =>
   createStyles({
     header: {
-      margin: theme.spacing(2, 0, 0, 0)
+      marginTop: theme.spacing(2)
     },
     divider: {
       border: theme.palette.grey[700] + " solid 1px ",
