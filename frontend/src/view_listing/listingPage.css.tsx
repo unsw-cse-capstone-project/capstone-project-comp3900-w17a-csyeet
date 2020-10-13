afa3eb532546d63282ff6473f0fbd824b3d57c52
Jeremy Chiu <jeremy.chiu26@gmail.com>
2020-10-14 10:53:49 +1100

    minor style changes

--- frontend/src/view_listing/listingPage.css.tsx
+++ frontend/src/view_listing/listingPage.css.tsx
@@ -5,45 +5,45 @@ export const listingPageStyle = makeStyles((theme: Theme) =>
     page: {
       padding: theme.spacing(2, "15%", 0, "15%"),
       display: "flex",
-      flexDirection: "column"
+      flexDirection: "column",
     },
     backButton: {
-      width: "fit-content"
+      width: "fit-content",
     },
     header: {
-      margin: theme.spacing(4, 0, 0, 0)
+      margin: theme.spacing(4, 0, 0, 0),
     },
     divider: {
       border: theme.palette.grey[700] + " solid 1px ",
       backgroundColor: theme.palette.grey[700],
-      margin: theme.spacing(0, 0, 1, 0)
+      marginBottom: theme.spacing(1),
     },
     imageContainer: {
       width: "100%",
       height: "500px",
       objectFit: "contain",
-      backgroundColor: "rgba(0,0,0, 0.8)"
+      backgroundColor: "rgba(0,0,0, 0.8)",
     },
     sliderContainer: {
       [theme.breakpoints.up("md")]: {
-        width: "100%"
+        width: "100%",
       },
-      height: "500px"
+      height: "500px",
     },
     street: {
-      padding: theme.spacing(2, 0, 0, 0)
+      padding: theme.spacing(2, 0, 0, 0),
     },
     title: {
-      margin: theme.spacing(4, 0, 1, 0)
+      margin: theme.spacing(4, 0, 1, 0),
     },
     description: {
-      margin: theme.spacing(0, 0, 3, 0)
+      margin: theme.spacing(0, 0, 3, 0),
     },
     detailBar: {
       display: "flex",
       margin: theme.spacing(1, 0, 0, 0),
       justifyContent: "flex-end",
-      padding: theme.spacing(2, 0, "15%", 0)
-    }
+      padding: theme.spacing(2, 0, "15%", 0),
+    },
   })
 );
--- frontend/src/view_listing/seller_profile/sellerProfile.css.tsx
+++ frontend/src/view_listing/seller_profile/sellerProfile.css.tsx
@@ -3,12 +3,12 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core";
 export const sellerProfileStyle = makeStyles((theme: Theme) =>
   createStyles({
     header: {
-      margin: theme.spacing(4, 0, 0, 0)
+      margin: theme.spacing(4, 0, 0, 0),
     },
     divider: {
       border: theme.palette.grey[700] + " solid 1px ",
       backgroundColor: theme.palette.grey[700],
-      margin: theme.spacing(0, 0, 1, 0)
-    }
+      marginBottom: theme.spacing(1),
+    },
   })
 );
