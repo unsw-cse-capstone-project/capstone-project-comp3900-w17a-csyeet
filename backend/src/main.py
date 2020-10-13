fd8eb23163336d8dad40de2f3b4240512e114d97
Eddie Belokopytov <eddie_491@hotmail.com>
2020-10-13 21:04:04 +1100

    Adds authentication and authorisation

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,5 +7,5 @@ app = FastAPI(title='Abode API')
 app.add_middleware(DBSessionMiddleware,
                    db_url='postgresql://postgres:password@localhost:5432/abode')  # maybe import from config in future (also update alembic.ini)
 
-app.include_router(auth.router, prefix='/auth', tags=['auth'])
+app.include_router(auth.router, tags=['authorisation'])
 app.include_router(listing.router, prefix='/listing', tags=['listing'])
--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,2 +1,2 @@
-from .user import SignupRequest, UserResponse
+from .user import SignupRequest, LoginRequest, UserResponse
 from .listing import ListingType, Feature, CreateListingRequest, ListingResponse, field_to_feature_map
