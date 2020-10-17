c521dd0543f1b7939cc64654561be8714afc5b9d
Eddie Belokopytov <eddie_491@hotmail.com>
2020-10-17 13:59:51 +1100

    Adds more filters and fixes Listing model type hints

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,3 +1,3 @@
 from .user import SignupRequest, LoginRequest, UserResponse
-from .listing import ListingType, Feature, CreateListingRequest, ListingResponse, field_to_feature_map, ListingSearchResponse, AuctionResponse
+from .listing import ListingType, Feature, CreateListingRequest, ListingResponse, field_to_feature_map, SearchListingsRequest, SearchListingsResponse, AuctionResponse
 from .registration import CreateRegistrationRequest, RegistrationResponse
