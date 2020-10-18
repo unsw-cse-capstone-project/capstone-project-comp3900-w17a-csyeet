70d2e32cd64a7825107ad54215e3f8299afb9160
Eddie Belokopytov <eddie_491@hotmail.com>
2020-10-18 11:47:13 +1100

    Checks if user is already an RAB when registering

--- backend/src/controllers/registration.py
+++ backend/src/controllers/registration.py
@@ -7,14 +7,21 @@ from ..models import Registration, Listing, User
 
 router = APIRouter()
 
-@router.post('/{listing_id}', response_model=RegistrationResponse, responses={404: {"description": "Resource not found"}})
+
+@router.post('/{listing_id}', response_model=RegistrationResponse,
+             responses={404: {"description": "Resource not found"}, 403: {"description": "Operation forbidden"}})
 def register(listing_id: int, req: CreateRegistrationRequest, current_user: User = Depends(get_current_user), session: Session = Depends(lambda: db.session)):
     ''' Registers a user as a RAB for a listing '''
     listing = session.query(Listing).get(listing_id)
     if listing is None:
         raise HTTPException(
             status_code=404, detail="Requested listing could not be found")
-    registration = Registration(listing_id = listing_id, user_id = current_user.id, **req.dict())
+    if session.query(Registration).get((listing_id, current_user.id)):
+        raise HTTPException(
+            status_code=403, detail="User already registered for listing")
+
+    registration = Registration(
+        listing_id=listing_id, user_id=current_user.id, **req.dict())
     session.add(registration)
     session.commit()
     return registration
