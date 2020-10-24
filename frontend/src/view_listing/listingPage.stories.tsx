655dd074115b9d8ee71f830f0f9c986f86f4feee
Teresa <miss.teresa.feng@gmail.com>
2020-10-24 14:30:43 +1100

    Starring a Property and Landmark integration (#33)

--- frontend/src/view_listing/listingPage.stories.tsx
+++ frontend/src/view_listing/listingPage.stories.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Meta, Story } from "@storybook/react/types-6-0";
 import { ListingPage } from "./listingPage";
 import { createFakeActualListing } from "../ui/util/fakes/listing";
+import { makeStyles, Theme, createStyles } from "@material-ui/core";
+import { ListingActual } from '../ui/util/types/listing';
 
 export default {
   title: "view_listing/listingPage",
   component: ListingPage,
   argTypes: {
+    listing: {
+      defaultValue: createFakeActualListing(),
+    },
     auction_start: {
       control: {
         type: "date",
@@ -22,15 +27,27 @@ export default {
   },
 } as Meta;
 
+const listingPageStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    page: {
+      padding: theme.spacing(2, "15%", 0, "15%"),
+      display: "flex",
+      flexDirection: "column",
+    },
+  }));
+
 export const Overview = (props: {
   auction_start: string;
   auction_end: string;
-}) => (
-  <ListingPage
-    listing={createFakeActualListing({
-      auction_start: new Date(props.auction_start),
-      auction_end: new Date(props.auction_end),
-    })}
-    SuburbPanelContent={() => <div>hello</div>}
-  />
-);
+  listing: ListingActual;
+}) => {
+  const classes = listingPageStyle();
+  props.listing.auction_start = new Date(props.auction_start);
+  props.listing.auction_end = new Date(props.auction_end);
+  return <div className={classes.page}>
+    <ListingPage
+      listing={props.listing}
+      SuburbPanelContent={() => <div>hello</div>}
+    />
+  </div>
+};
