6640bcd088bf2db085b5187ad67fe1f59a0f4e08
Teresa Feng <teresa.f@canva.com>
2020-11-02 09:18:39 +1100

    wip

--- frontend/src/home/main.tsx
+++ frontend/src/home/main.tsx
@@ -5,20 +5,29 @@ import { SearchBar } from "../ui/base/search_bar/SearchBar";
 import { SearchStore } from "../search/SearchPresenter";
 import { useHistory } from "react-router-dom";
 import { HomePageStyle } from "./HomePage.css";
+import { createUpcomingAuction } from './upcoming_auctions/create';
 
 export const HomePage = () => {
   const store = new SearchStore();
   const history = useHistory();
   const onSubmit = () => history.push("/search?query=" + store.input);
   const classes = HomePageStyle();
+  const UpcomingAuctions = createUpcomingAuction();
   return (
     <div className={classes.page}>
+      <div className={classes.top}>
       <img width="250px" src={logo} alt="Adobe logo"></img>
-      <Typography variant="h4" style={{ margin: "15px 0" }}>
+      <Typography variant="h5" style={{ margin: "15px 0" }}>
         Find your next Abode
       </Typography>
       <div className={classes.searchBar}>
         <SearchBar store={store} onSubmit={onSubmit} />
+      </div>
+      <div className={classes.background}></div>
+      </div>
+      <div className={classes.upcomingAuctionContainer}>
+        <Typography variant="h5" className={classes.upcomingAuctionTitle}>Upcoming Auctions</Typography>
+        <UpcomingAuctions />
       </div>
     </div>
   );
