61af875ea6a460f5d6d4350dac56ae069e922e71
Jeremy Chiu <jeremy.chiu26@gmail.com>
2020-11-04 00:09:24 +1100

    wip profile modal

--- frontend/src/view_listing/seller_profile/sellerProfile.tsx
+++ frontend/src/view_listing/seller_profile/sellerProfile.tsx
@@ -4,11 +4,13 @@ import {
   Divider,
   Avatar,
   Dialog,
+  DialogContent,
   DialogTitle,
   Link,
 } from "@material-ui/core";
 import { sellerProfileStyle } from "./sellerProfile.css";
 import { ListingActual } from "../../ui/util/types/listing";
+import { getListingFromResult } from '../../ui/util/helper';
 
 export const SellerProfile = ({
   id,
@@ -30,6 +32,16 @@ export const SellerProfile = ({
     setOpen(false);
   };
 
+  const [blurb, setBlurb] = React.useState(undefined);
+  const [listings, setListings] = React.useState(undefined);
+
+  React.useEffect(() => {
+    getInfoFromProfile(id).then((r) => {
+      setBlurb(r.blurb);
+      setListings(r.listings);
+    });
+  }, []);
+
   return (
     <div>
       <Typography variant="h5" className={classes.header}>
@@ -37,13 +49,19 @@ export const SellerProfile = ({
       </Typography>
       <Divider className={classes.divider} />
       <div style={{ display: "flex", alignItems: "center" }}>
-        <Avatar src="https://miro.medium.com/max/2560/1*gBQxShAkxBp_YPb14CN0Nw.jpeg"></Avatar>
+        <Avatar src="https://miro.medium.com/max/2560/1*gBQxShAkxBp_YPb14CN0Nw.jpeg" />
         <Typography variant="body1" style={{ paddingLeft: "10px" }}>
           <Link onClick={handleClickOpen}>{name}</Link>
         </Typography>
       </div>
       {children}
-      <ProfileDialog name={name} open={open} onClose={handleClose} />
+      <ProfileDialog
+        blurb={blurb}
+        listings={listings}
+        name={name}
+        open={open}
+        onClose={handleClose}
+      />
     </div>
   );
 };
@@ -57,11 +75,38 @@ function ProfileDialog(props: {
 }) {
   return (
     <Dialog
-      onclose={onClose}
+      onClose={props.onClose}
       aria-labelledby="profile-dialog-title"
-      open={open}
+      open={props.open}
     >
-      <DialogTitle id="profoile-dialog-title">{name}</DialogTitle>
+      <DialogTitle id="profoile-dialog-title">Seller Profile</DialogTitle>
+      <DialogContent dividers>
+        <div style={{ display: "flex" }}>
+          <Avatar src="https://miro.medium.com/max/2560/1*gBQxShAkxBp_YPb14CN0Nw.jpeg" />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Typography>{props.name}</Typography>
+            <Typography>{props.blurb}</Typography>
+          </div>
+        </div>
+        <div style={{ paddingTop: "20px" }}>
+          <Typography>{props.name}'s Listings</Typography>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 }
+
+const getInfoFromProfile = async (user_id: number) => {
+  const response = await fetch(`/users/${user_id}/profile`);
+  const result = await response.json();
+  if ("detail" in result) {
+    return undefined;
+  }
+
+  const obj = {
+    blurb: String = result.blurb,
+    listings: ListingActual[] = result.listings.map((result: any) => getListingFromResult(result)),
+  }
+
+  return obj;
+};
