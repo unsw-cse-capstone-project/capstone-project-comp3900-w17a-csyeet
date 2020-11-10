277b7c90d2ee4ed7ea8fe02e360762b0da1c0551
annisaPutrideva <52066293+annisaPutrideva@users.noreply.github.com>
2020-11-11 09:47:31 +1100

    Update backend/src/controllers/listings.py
    Co-authored-by: edzobel491 <edzo491@gmail.com>

--- backend/src/controllers/listings.py
+++ backend/src/controllers/listings.py
@@ -258,7 +258,7 @@ def update(id: int, req: UpdateListingRequest = Depends(), signed_in_user: User 
         raise HTTPException(
             status_code=403, detail="User cannot edit this listing") 
     
-    if any(x is not None for x in [req.auction_start, req.auction_end, req.reserve_price, req.account_name, req.bsb, req.account_number]) and listing.auction_start < datetime.now():
+    if listing.auction_start < datetime.now() and any([req.auction_start, req.auction_end, req.reserve_price, req.account_name, req.bsb, req.account_number]):
         raise HTTPException(
             status_code=403, detail="Cannot update auction details once auction has started")
     
