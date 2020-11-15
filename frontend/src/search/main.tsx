0fdaeba45538d7d288200bed1d379001c9ec8439
Teresa Feng <teresa.f@canva.com>
2020-11-15 14:45:10 +1100

    removed unused import

--- frontend/src/search/main.tsx
+++ frontend/src/search/main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Redirect, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { createSearchPage } from "./create";
 import { ErrorBoundaryPage } from "../ui/base/error_boundary/ErrorBoundary";
 
